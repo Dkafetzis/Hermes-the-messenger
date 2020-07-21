9023dac88f52e012e715d33d7bb0b9f92ef9f65c
Dkafetzis <dimitris.kafetzis00@gmail.com>
2020-07-21 18:52:17 +0300

    Subscribe works for only one topic

--- src/main/java/hackathon/quarkus/resources/DeviceResource.java
+++ src/main/java/hackathon/quarkus/resources/DeviceResource.java
@@ -1,5 +1,6 @@
 package hackathon.quarkus.resources;
 
+import java.nio.charset.StandardCharsets;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -11,67 +12,50 @@ import javax.ws.rs.core.Response;
 
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 import hackathon.quarkus.model.Device;
-import hackathon.quarkus.service.DeviceService;
 import io.smallrye.reactive.messaging.mqtt.MqttMessage;
 
-@Path("/publish")
+@Path("/Resources")
 public class DeviceResource {
 
-    @GET
+	static String message = new String();
+	
+	@GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getMessages(){
-        return Response.status(Response.Status.OK).entity(DeviceService.initList()).build();
+        return Response.status(Response.Status.OK).entity("Hello").build();
     }
 
     @Inject
-    @Channel("panatha")
+    @Channel("out")
     Emitter<String> emitter;
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response createUser(Device device) {
+    @Path("/Publish")
+    public Response publish(Device device) {
         System.out.println("New Publish request: message->"+device.getMessage()+" & topic->"+device.getTopic());
         //SendingMqttMessage<String> message = new SendingMqttMessage<>("myTopic","A message in here",0,false);
         emitter.send(MqttMessage.of(device.getTopic(), device.getMessage()));
 
-
-        /*MqttMessage<String> mess = new MqttMessage<>() {
-            @Override
-            public int getMessageId() {
-                return 0;
-            }
-
-            @Override
-            public MqttQoS getQosLevel() {
-                return null;
-            }
-
-            @Override
-            public boolean isDuplicate() {
-                return false;
-            }
-
-            @Override
-            public boolean isRetain() {
-                return false;
-            }
-
-            @Override
-            public String getTopic() {
-                return null;
-            }
-
-            @Override
-            public String getPayload() {
-                return null;
-            }
-        };
-         */
-
-
         return Response.ok().status(Response.Status.CREATED).build();
+    }
+    
+    
+    @Incoming("in")
+    public void consume(byte [] raw) {
+    	message = new String(raw, StandardCharsets.UTF_8);
+        System.out.println("Message received: " + message);
+    }
+    
+    @GET
+    @Path("/Subscribe")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response subscribe() {
+    	System.out.println(message);
+        return Response.ok().status(Response.Status.ACCEPTED).entity("{ \"message\":\"" + message + "\"}").build();
     }
 }
